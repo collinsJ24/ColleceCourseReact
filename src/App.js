f4b92acf2777d0e5fad392369a42c0fd883f690c
collinsJ24 <james.collins36@mail.dcu.ie>
2021-06-21 21:48:38 +0100

    new endpoints and css change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Select from 'react-select'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      selectOptions : [],
+      course_code: '',
+      course_name: ''
+    }
+  }
+
+ async getOptions(){
+    const res = await axios.get('http://localhost:8080/college/DCU')
+    const data = res.data
+
+    const options = data.map(d => ({
+      "value" : d.course_name,
+      "label" : d.course_code
+
+    }))
+
+    this.setState({selectOptions: options})
+
+  }
+
+  async getOptions2(requestVar){
+      const res = await axios.get('http://localhost:8080/college/' + requestVar)
+      const data = res.data
+
+      const options = data.map(d => ({
+        "value" : d.email,
+        "label" : d.userId
+
+      }))
+
+      this.setState({selectOptions: options})
+
+    }
+
+  handleChange(e){
+   this.setState({course_code:e.value, course_name:e.label})
+   this.getOptions2("test")
+  }
+
+  componentDidMount(){
+      this.getOptions()
+  }
+
+  render() {
+    console.log(this.state.selectOptions)
+    return (
+      <div >
+        <Select className="dropdown" options={this.state.selectOptions} onChange={this.handleChange.bind(this)} />
+    <p>You have selected <strong>{this.state.course_code}</strong> whose id is <strong>{this.state.course_name}</strong></p>
+      </div>
+    )
+  }
 }
-
-export default App;
